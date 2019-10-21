db27dee93a2313d2dd9b05c62140971cdeee31a0
walternascimentobarroso <walter.nascimento.barroso@gmail.com>
2019-10-21 11:17:55 -0400

    [ADD] Addlaravel mix

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -1,11 +1,11 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <div id="app">
-    <example>
-        Carregando
-    </example>
+    <example-component>
+        Carregandod
+    </example-component>
 </div>
 
 <script src="/js/manifest.js"></script>
 <script src="/js/vendor.js"></script>
 <script src="/js/boostrap.js"></script>
-{{-- <script src="/js/app.js"></script> --}}
+<script src="/js/threads.js"></script>
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/bootstrap.js', 'public/js/bootstrap.js')
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
+mix.js('resources/js/threads/app.js', 'public/js/threads.js')
+mix.js('resources/js/replies/app.js', 'public/js/replies.js')
+mix.sass('resources/sass/app.scss', 'public/css')
     .extract(['vue']);
